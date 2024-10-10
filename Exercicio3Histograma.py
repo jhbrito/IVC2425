b17adfa19b146e69a3444ddea7aca6555a3efae5
jhbrito <jbrito@ipca.pt>
2024-10-10 09:56:33 +0100

    Exercicio 3

--- Exercicio3Histograma.py
+++ Exercicio3Histograma.py
@@ -5,14 +5,15 @@ import numpy as np
 
 
 folder = "Files"
-image = cv2.imread(os.path.join(folder, "baboon.png"))
-# image = cv2.imread(os.path.join(folder, "cao.jpg"))
-# image = cv2.imread(os.path.join(folder, "lena.png"))
-# image = cv2.imread(os.path.join(folder, "moedas.jpg"))
-# image = cv2.imread(os.path.join(folder, "PET-Body-02.jpg"))
-# image = cv2.imread(os.path.join(folder, "Sharbat_Gula.jpg"))
+images = []
+images.append("baboon.png")
+images.append("cao.jpg")
+images.append("lena.png")
+images.append("moedas.jpg")
+images.append("PET-Body-02.jpg")
+images.append("Sharbat_Gula.jpg")
 
-image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
 #cv2.imshow("image_gray", image_gray)
 
 def ivc_histogram(src):
@@ -22,9 +23,6 @@ def ivc_histogram(src):
         hist[i] = m_i
     return hist
 
-image_hist = ivc_histogram(image_gray)
-pdf = image_hist / (image_gray.shape[0] * image_gray.shape[1])
-
 def ivc_pdf_2_cdf(pdf):
     cdf = np.zeros((256,), dtype=np.float64)
     cdf[0] = pdf[0]
@@ -32,34 +30,48 @@ def ivc_pdf_2_cdf(pdf):
         cdf[i] = cdf[i-1] + pdf[i]
     return cdf
 
-cdf = ivc_pdf_2_cdf(pdf)
+def equalize(f, cdf):
+    cdfmin = cdf[0]
+    g = np.zeros(image_gray.shape, dtype=image_gray.dtype)
+    for y in range(g.shape[0]):
+        for x in range(g.shape[1]):
+            g[y, x] = ((cdf[f[y, x]] - cdfmin) / 1 - cdfmin) * 255
+    return g
 
-f = image_gray
-cdfmin = cdf[0]
-g = np.zeros(image_gray.shape, dtype=image_gray.dtype)
-for y in range(g.shape[0]):
-    for x in range(g.shape[1]):
-        g[y, x] = ((cdf[f[y, x]] - cdfmin) / 1-cdfmin) * 255
 
+for image_name in images:
+    image = cv2.imread(os.path.join(folder, image_name))
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-image2_hist = ivc_histogram(g)
-pdf2 = image2_hist / (g.shape[0] * g.shape[1])
-cdf2 = ivc_pdf_2_cdf(pdf2)
+    image_hist = ivc_histogram(image_gray)
+    pdf = image_hist / (image_gray.shape[0] * image_gray.shape[1])
 
-plt.subplot(2, 3, 1)
-plt.imshow(cv2.cvtColor(image_gray, cv2.COLOR_GRAY2RGB))
-plt.subplot(2, 3, 2)
-plt.plot(pdf)
-plt.subplot(2, 3, 3)
-plt.plot(cdf)
-plt.subplot(2, 3, 4)
-plt.imshow(cv2.cvtColor(g, cv2.COLOR_GRAY2RGB))
-plt.subplot(2, 3, 5)
-plt.plot(pdf2)
-plt.subplot(2, 3, 6)
-plt.plot(cdf2)
+    cdf = ivc_pdf_2_cdf(pdf)
+    g = equalize(image_gray, cdf)
 
-plt.show()
+    image2_hist = ivc_histogram(g)
+    pdf2 = image2_hist / (g.shape[0] * g.shape[1])
+    cdf2 = ivc_pdf_2_cdf(pdf2)
+
+    plt.subplot(2, 3, 1)
+    plt.imshow(cv2.cvtColor(image_gray, cv2.COLOR_GRAY2RGB))
+    plt.title("Imagem original")
+    plt.subplot(2, 3, 2)
+    plt.plot(pdf)
+    plt.title("PDF")
+    plt.subplot(2, 3, 3)
+    plt.plot(cdf)
+    plt.title("CDF")
+    plt.subplot(2, 3, 4)
+    plt.imshow(cv2.cvtColor(g, cv2.COLOR_GRAY2RGB))
+    plt.title("Imagem equalizada")
+    plt.subplot(2, 3, 5)
+    plt.plot(pdf2)
+    plt.title("PDF equalizada")
+    plt.subplot(2, 3, 6)
+    plt.plot(cdf2)
+    plt.title("CDF equalizada")
+    plt.show()
 
 
 #cv2.waitKey(0)
